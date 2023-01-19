905aa1f6f8a2d1a152606f3c30d2e10395bb03e9
FoubaDev <foubabertrand@gmail.com>
2023-01-19 12:45:11 +0100

    add file for database connection

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 hello world
 ajout de la deuxime ligne
-<<<<<<< HEAD
 make by second branch
-=======
-add from julie
-add from mahamat
->>>>>>> 640eb1aa9d3233181fc73197504c330799083247
---
+++ database.py
@@ -0,0 +1,3 @@
+# this file is used to configure the connection with the database
+
+# we will use postgresql to keep all data coming from form 
